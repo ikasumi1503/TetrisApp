3a176e73720a8c65c05cd66b9e8b8132757a3854
ikasumi1503 <anohianotokik@yahoo.co.jp>
2025-05-09 21:10:40 +0900

    一時保存

--- app/src/main/java/com/example/tetrisapp/feature_game/data/GameRepository.kt
+++ app/src/main/java/com/example/tetrisapp/feature_game/data/GameRepository.kt
@@ -6,9 +6,12 @@ import com.example.tetrisapp.feature_game.domain.entity.TetriMino
 // Implで使うメソッドなどの型を決めておく「抽象」的な部分を宣言する役割
 
 // GameRepositoryの役割は？
-// ゲームのドメインロジックやデータソースをimportしてまとめてViewModelで使う窓口
+// 外部とのやり取りをする窓口
 
-//
+// やることの例
+// APIで受け取った物をuseCaseに渡す
+// ローカルにデータを保存したり、読み込んだりする処理
+// データ型の変換(ドメインロジックで頻繁にデータの変換を行う場合はUseCase内で、API通信などのためにデータの型を変換する場合にはレポジトリ内で変換を行う)
 interface GameRepository {
     fun moveDown(): TetriMino
 }
