4ee9750fc392b46445268e9849c7a4185b8b458e
ikasumi1503 <anohianotokik@yahoo.co.jp>
2025-05-15 13:53:58 +0900

    使っていない定数などを捨てた

--- app/src/main/java/com/example/tetrisapp/feature_game/util/GameConstants.kt
+++ app/src/main/java/com/example/tetrisapp/feature_game/util/GameConstants.kt
@@ -3,8 +3,6 @@ package com.example.tetrisapp.feature_game.util
 // object classはインスタンスを一つだけ作る
 // クラスに属さない共通で使うものをまとめる時に使ったりするみたい
 object GameConstants {
-    private const val MINO_WIDTH = 4
-    private const val BOARD_WIDTH = 10
 
     // constはString型とかシンプルな型にしか適用できない
     val JLSTZ_KickTable = mapOf(
